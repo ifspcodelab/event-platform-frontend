ecd8a5c4b6a9d9d9373d377b4617269fc5527852
Lucas Gabriel <lucasogjc@gmail.com>
2022-07-26 16:31:37 -0300

    feat: adds login component and its routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './site/pages/home/home.component';
 import { RegistrationComponent } from './site/pages/registration/registration.component';
+import { LoginComponent } from "./site/pages/login/login.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cadastro', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
 ];
 
--- src/app/site/site.module.ts
+++ src/app/site/site.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule
