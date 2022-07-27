c7446e628ed72243f4d03bfd08eb99ebde8fe849
rafaelspa <rsdepaula67@gmail.com>
2022-07-27 16:09:14 -0300

    feat: add registration models

---
+++ src/app/core/models/account-create-dto.ts
@@ -0,0 +1,7 @@
+export interface AccountCreateDto {
+  name: string;
+  email: string;
+  cpf: string;
+  password: string;
+  agreed: boolean;
+}
---
+++ src/app/core/models/account-role.ts
@@ -0,0 +1,6 @@
+export enum AccountRole {
+    ADMIN = ("ADMIN"),
+    COLLABORATOR = ("COLLABORATOR"),
+    SPEAKER = ("SPEAKER"),
+    ATTENDANT = ("ATTENDANT")
+}
