2f0a070ccfc2200feac0136b67fa4e4ee272acb5
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-07-27 11:01:54 -0300

    feat: add pipe to subevent status and dates

--- src/app/core/services/subevent.service.ts
+++ src/app/core/services/subevent.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {environment} from "../../../environments/environment";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {EventDto} from "../models/event.model";
 import {SubeventDto} from "../models/subevent.model";
 
 @Injectable({
