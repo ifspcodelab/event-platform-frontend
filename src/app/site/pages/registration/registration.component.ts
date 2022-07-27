a10482895b0f903fa431833b16f19af7b3c0a568
rafaelspa <rsdepaula67@gmail.com>
2022-07-27 16:10:38 -0300

    feat: add registration form

--- src/app/site/pages/registration/registration.component.ts
+++ src/app/site/pages/registration/registration.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { RegistrationService } from 'src/app/core/services/registration.service';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +9,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  form: FormGroup = this.buildForm();
 
-  constructor() { }
+  constructor(
+    private registrationService: RegistrationService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
 
+  buildForm(): FormGroup {
+    return this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(256)]],
+      email: [null],
+      cpf: [null],
+      password: [null],
+      agreed: [null]
+    });
+  }
+
+  get name() { return this.form.get('name')!; }
+
+  field(path: string) {
+    return this.form.get(path)!;
+  }
+
+  fieldErrors(path: string) {
+    return this.field(path)?.errors;
+  }
+
+  onSubmit() {
+    if(this.form.invalid) {
+      return;
+    }
+    this.createAccount();
+    console.log(this.form);
+  }
+
+  createAccount() {
+    this.registrationService.postAccount(this.form.value)
+      .pipe(first())
+      .subscribe(
+        accountDto => console.log(accountDto)
+      );
+  }
 }
+
+
--- src/app/site/site.module.ts
+++ src/app/site/site.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    RegistrationComponent
+    RegistrationComponent,
+
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class SiteModule { }
