5aad97d00ebed9cde5f2783292850ba1897c829f
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-07-27 10:22:02 -0300

    feat: formats the event list, event details, and subevent list dates

--- src/app/admin/events/events.module.ts
+++ src/app/admin/events/events.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 import { EventsRoutingModule } from './events-routing.module';
 import { EventsComponent } from './events.component';
 import { EventListComponent } from './events/event-list/event-list.component';
 import { EventShowComponent } from './events/event-show/event-show.component';
 import { EventStatusPipe } from "../../core/pipes/event-status.pipe";
+import { SubeventShowComponent } from './subevents/subevent-show/subevent-show.component';
 
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { SubeventShowComponent } from './subevents/subevent-show/subevent-show.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +24,7 @@ import { SubeventShowComponent } from './subevents/subevent-show/subevent-show.c
   ],
   imports: [
     CommonModule,
+    DatePipe,
     EventsRoutingModule,
     MatTableModule,
     MatButtonModule,
