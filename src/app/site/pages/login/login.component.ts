614f70b50ac23f179c81aaea209dab56af9ced74
Lucas Gabriel <lucasogjc@gmail.com>
2022-07-27 19:35:16 -0300

    feat: adds password and email validation

--- src/app/site/pages/login/login.component.ts
+++ src/app/site/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import { AuthenticationService } from "../../../core/services/authentication.service";
+import {AppValidators} from "../../../core/validators/app-validator";
 
 @Component({
   selector: 'app-login',
@@ -20,12 +21,20 @@ export class LoginComponent implements OnInit {
 
   private buildForm() {
     return this.formBuilder.group({
-      email: [''],
-      password: ['']
+      email: ['', [Validators.required, AppValidators.notBlank, Validators.email]],
+      //TODO: verificar "Validators.email" x regex;
+      //TODO: verificar "Validators.required"
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(64)]]
     });
   }
 
+  get email() { return this.form.get('email')!; }
+  get password() { return this.form.get('password')!; }
+
   onSubmit() {
+    if(this.form.invalid) {
+      return;
+    }
     console.log("form has been submitted");
     this.login();
   }
