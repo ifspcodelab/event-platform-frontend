04f6a0b700824fde9827bc27bf81de4c8aeac2e5
isabella-66 <amasprog@gmail.com>
2022-07-29 01:26:41 -0300

    fix: update location list and scss file

--- src/app/admin/locations/locations/location-list/location-list.component.ts
+++ src/app/admin/locations/locations/location-list/location-list.component.ts
@@ -4,6 +4,7 @@ import { LocationService } from 'src/app/core/services/location.service';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { LocationFormComponent } from '../location-form/location-form.component';
+import { NotificationService } from 'src/app/core/services/notification.service';
 
 @Component({
   selector: 'app-location-list',
@@ -18,14 +19,15 @@ export class LocationListComponent implements OnInit {
 
   constructor(
     private locationService: LocationService,
+    private notificationService: NotificationService,
     private router: Router,
     public dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
     this.locationService.getLocations().subscribe(
-      locationsDto => {
-        this.locationsDto = locationsDto;
+      locations => {
+        this.locationsDto = locations;
       }
     )
   }
@@ -44,11 +46,13 @@ export class LocationListComponent implements OnInit {
     };
   }
 
-  openFormLocationDialog() {
+  openAddLocationFormDialog() {
     const dialogRef = this.dialog.open(LocationFormComponent, this.getDialogConfig());
+
     dialogRef.afterClosed().subscribe(locationDto => {
       if (locationDto) {
         this.locationsDto = [...this.locationsDto, locationDto];
+        this.notificationService.success("Cadastrado com sucesso");
       }
     });
   }
