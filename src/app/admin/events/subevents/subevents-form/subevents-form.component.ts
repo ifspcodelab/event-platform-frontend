9925ef58eddeffb1a710de1842d117e13b926f8b
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-07-30 08:24:01 -0300

    refactor: change subevent form component

--- src/app/admin/events/subevents/subevents-form/subevents-form.component.ts
+++ src/app/admin/events/subevents/subevents-form/subevents-form.component.ts
@@ -3,12 +3,12 @@ import { NotificationService } from './../../../../core/services/notification.se
 import { ProblemDetail } from './../../../../core/models/problem-detail';
 import { AppValidators } from 'src/app/core/validators/app-validator';
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {SubeventService} from "../../../../core/services/subevent.service";
-import {ActivatedRoute, Router} from "@angular/router";
-import {first} from "rxjs";
-import {HttpErrorResponse} from "@angular/common/http";
-import {Violation} from "../../../../core/models/problem-detail";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { SubeventService } from "../../../../core/services/subevent.service";
+import {ActivatedRoute, Router } from "@angular/router";
+import { first } from "rxjs";
+import { HttpErrorResponse } from "@angular/common/http";
+import { Violation } from "../../../../core/models/problem-detail";
 
 @Component({
   selector: 'app-subevents-form',
@@ -16,7 +16,7 @@ import {Violation} from "../../../../core/models/problem-detail";
   styleUrls: ['./subevents-form.component.scss']
 })
 export class SubeventsFormComponent implements OnInit {
-  form: FormGroup = this.buildForm();
+  form: FormGroup;
   eventId: string;
   submitted: boolean = false;
   subeventDto: SubeventDto;
@@ -24,14 +24,15 @@ export class SubeventsFormComponent implements OnInit {
   createMode: boolean;
 
   constructor(
-    private formBuilder: FormBuilder,
     private subeventService: SubeventService,
+    private notificationService: NotificationService,
+    private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-    private notificationService: NotificationService
   ) { }
 
   ngOnInit(): void {
+    this.form = this.buildForm();
     this.eventId = this.route.snapshot.paramMap.get('eventId');
     this.subeventId = this.route.snapshot.paramMap.get('subeventId');
 
@@ -51,15 +52,36 @@ export class SubeventsFormComponent implements OnInit {
           this.subeventDto = subeventDto;
           this.form.patchValue(subeventDto);
         }
-      )
+      );
   }
 
-  buildForm(): FormGroup {
+  private buildForm(): FormGroup {
     return this.formBuilder.group({
-      title: ['', [Validators.required, AppValidators.notBlank, Validators.minLength(3), Validators.maxLength(50)]],
+      title: ['', 
+        [
+          Validators.required, 
+          AppValidators.notBlank, 
+          Validators.minLength(3), 
+          Validators.maxLength(50)
+        ]
+      ],
       slug: ['', [Validators.required, AppValidators.notBlank]],
-      summary: ['', [Validators.required, AppValidators.notBlank, Validators.minLength(100), Validators.maxLength(150)]],
-      presentation: ['', [Validators.required, AppValidators.notBlank, Validators.minLength(1000), Validators.maxLength(5000)]],
+      summary: ['', 
+        [
+          Validators.required,
+          AppValidators.notBlank,
+          Validators.minLength(100),
+          Validators.maxLength(150)
+        ]
+      ],
+      presentation: ['', 
+        [
+          Validators.required,
+          AppValidators.notBlank,
+          Validators.minLength(1000),
+          Validators.maxLength(5000)
+        ]
+      ],
       executionPeriod: this.formBuilder.group({
         startDate: ['', [Validators.required]],
         endDate: ['',[Validators.required]]
@@ -71,15 +93,14 @@ export class SubeventsFormComponent implements OnInit {
 
   onSubmit(){
     this.submitted = true;
+
     if(this.form.invalid) {
       return;
     }
 
     if(this.createMode) {
       this.createSubevent();
-    }
-
-    else {
+    } else {
       this.updateSubevent();
     }
   }
@@ -87,62 +108,25 @@ export class SubeventsFormComponent implements OnInit {
   createSubevent() {
       this.subeventService.postSubevent(this.eventId, this.form.value)
         .pipe(first())
-        .subscribe(
-          subeventDto => {
-            if(subeventDto) {
-              this.notificationService.success('Cadastrado com sucesso');
+        .subscribe({
+          next: subeventDto => {
+              this.notificationService.success('Subevento cadastrado com sucesso');
               this.router.navigate(['admin', 'events', this.eventId, 'sub-events', subeventDto.id]);
-              console.log(subeventDto)
-            }
           },
-          error => this.handleError(error)
-        );
+          error:  error => this.handleError(error)
+        });
   }
 
   updateSubevent() {
     this.subeventService.putSubevent(this.eventId, this.form.value, this.subeventId)
         .pipe(first())
-        .subscribe(
-          subeventDto => {
-            if(subeventDto) {
-              this.notificationService.success('Editado com sucesso');
-              this.router.navigate(['admin', 'events', this.eventId, 'sub-events', subeventDto.id]);
-            }
+        .subscribe({
+          next: subeventDto => {
+            this.notificationService.success('Subevento editado com sucesso');
+            this.router.navigate(['admin', 'events', this.eventId, 'sub-events', subeventDto.id]);
           },
-          error => this.handleError(error)
-        );
-  }
-
-  fieldErrors(path: string) {
-    return this.field(path)?.errors;
-  }
-
-  field(path: string) {
-    return this.form.get(path)!;
-  }
-
-  handleError(error: any) {
-    if(error instanceof HttpErrorResponse) {
-      console.log(error);
-
-      if(error.status === 400) {
-        const violations: Violation[] = error.error;
-        violations.forEach(violation => {
-          const formControl = this.form.get(violation.name);
-          if(formControl) {
-            formControl.setErrors({
-              serverError: violation.message
-            });
-            console.log(formControl);
-          }
-        })
-      }
-
-      if(error.status == 409) {
-        const problem: ProblemDetail = error.error;
-        this.notificationService.error(problem.violations[0].message);
-      }
-    }
+          error: error => this.handleError(error)
+        });
   }
 
   getBackUrl() {
@@ -153,4 +137,41 @@ export class SubeventsFormComponent implements OnInit {
     }
   }
 
+  handleError(error: any) {
+    if(error instanceof HttpErrorResponse) {
+      if(error.status === 400) {
+        const violations: Violation[] = error.error;
+        violations.forEach(violation => {
+          const formControl = this.form.get(violation.name);
+          if(formControl) {
+            formControl.setErrors({ serverError: violation.message });
+          }
+        })
+      }
+
+      if(error.status === 409) {
+        const problem: ProblemDetail = error.error;
+        if(problem.title === "Resource already exists exception") {
+          // Depois será melhorado
+          const violation = problem.violations[0];
+          if(violation.message.includes("title")) {
+            this.form.get("title").setErrors({ serverError: 'Já existe um subevento com esse título' });
+          }
+          if(violation.message.includes("slug")) {
+            this.form.get("slug").setErrors({ serverError: 'Já existe um subevento com esse slug' });
+          }
+        } else {
+          this.notificationService.error(problem.violations[0].message);
+        }
+      }
+    }
+  }
+
+  fieldErrors(path: string) {
+    return this.field(path)?.errors;
+  }
+
+  field(path: string) {
+    return this.form.get(path)!;
+  }
 }
