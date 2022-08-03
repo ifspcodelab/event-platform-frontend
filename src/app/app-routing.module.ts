43b9896b5c5d51c2fa0e78b7c9abd8505f2e8de5
rafaelspa <rsdepaula67@gmail.com>
2022-08-03 16:56:47 -0300

    feat: add registration verify component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './site/pages/home/home.component';
 import { RegistrationComponent } from './site/pages/registration/registration.component';
 import { LoginComponent } from "./site/pages/login/login.component";
 import { TermsComponent } from "./site/pages/terms/terms.component";
+import { RegistrationVerifyComponent } from "./site/pages/registration-verify/registration-verify.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cadastro', component: RegistrationComponent },
+  { path: 'cadastro/verificacao/:token', component: RegistrationVerifyComponent },
   { path: 'login', component: LoginComponent },
   { path: 'termos', component: TermsComponent },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
---
+++ src/app/site/pages/registration-verify/registration-verify.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-verify',
+  templateUrl: './registration-verify.component.html',
+  styleUrls: ['./registration-verify.component.scss']
+})
+export class RegistrationVerifyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
