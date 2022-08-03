b388d6f1c676b58826b3deeaf0c753157e09b7f8
Domingos Latorre <domingos.latorre@gmail.com>
2022-08-03 15:28:25 -0300

    feat: add speaker module

---
+++ src/app/admin/speakers/speaker-list/speaker-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-speaker-list',
+  templateUrl: './speaker-list.component.html',
+  styleUrls: ['./speaker-list.component.scss']
+})
+export class SpeakerListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
