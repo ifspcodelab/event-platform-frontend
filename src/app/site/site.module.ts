d8a0e11932996f7f47f4af99736cb40ec939150f
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-08-03 19:14:45 -0300

    feat: create site footer

--- src/app/site/site.module.ts
+++ src/app/site/site.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { SiteHeaderComponent } from './components/site-header/site-header.component';
 import { SharedModule } from "../shared/shared.module";
+import { SiteFooterComponent } from './components/site-footer/site-footer.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     RegistrationComponent,
-    SiteHeaderComponent
+    SiteHeaderComponent,
+    SiteFooterComponent
   ],
   imports: [
     CommonModule,
