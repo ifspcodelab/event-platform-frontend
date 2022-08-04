cd71d209aae9d1e9c8b91a597ce56e0249827767
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-08-04 12:18:19 -0300

    feat: create methods to group the events in the home page

--- src/app/site/pages/home/home.component.ts
+++ src/app/site/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from "../../../core/services/event.service";
+import { EventDto } from "../../../core/models/event.model";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  eventsRegistrationDto: EventDto[];
+  eventsExecutionDto: EventDto[];
+  eventsFinishDto: EventDto[];
 
-  constructor() { }
+  constructor(private eventService: EventService) { }
 
   ngOnInit(): void {
+    this.eventService.getEvents()
+      .subscribe(events => {
+        this.getEventsWithRegistrationPeriodStarted(events);
+        this.getEventsWithExecutionPeriodStarted(events);
+        this.getEventsWithExecutionPeriodFinished(events);
+      });
   }
 
+  getEventsWithRegistrationPeriodStarted(events: EventDto[]) {
+    this.eventsRegistrationDto = events
+      .filter(
+        e => (new Date(e.registrationPeriod.startDate) <= new Date(Date.now())) &&
+          (new Date(e.executionPeriod.endDate) >= new Date(Date.now()))
+      );
+  }
+
+  getEventsWithExecutionPeriodStarted(events: EventDto[]) {
+    this.eventsExecutionDto = events
+      .filter(
+        e => (new Date(e.executionPeriod.startDate) <= new Date(Date.now())) &&
+          (new Date(e.executionPeriod.endDate) >= new Date(Date.now()))
+      );
+  }
+
+  getEventsWithExecutionPeriodFinished(events: EventDto[]) {
+    this.eventsFinishDto = events
+      .filter(e => new Date(e.executionPeriod.endDate) < new Date(Date.now()))
+  }
 }
