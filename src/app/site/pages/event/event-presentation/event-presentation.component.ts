3533228fef6f8301469f9ba0cd8b3b76abaaee63
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-08-04 21:34:45 -0300

    fix: change the style of footer

--- src/app/site/pages/event/event-presentation/event-presentation.component.ts
+++ src/app/site/pages/event/event-presentation/event-presentation.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import {EventDto} from "../../../../core/models/event.model";
-import {first} from "rxjs";
-import {SubeventService} from "../../../../core/services/subevent.service";
-import {EventService} from "../../../../core/services/event.service";
+import { EventDto } from "../../../../core/models/event.model";
+import { first } from "rxjs";
+import { EventService } from "../../../../core/services/event.service";
 
 @Component({
   selector: 'app-event-presentation',
@@ -22,9 +21,6 @@ export class EventPresentationComponent implements OnInit {
   ngOnInit(): void {
     this.eventSlug = this.route.snapshot.paramMap.get('eventSlug');
     this.fetchEvent();
-
-    document.getElementById("text")
-      .innerHTML = this.eventDto.presentation;
   }
 
   fetchEvent() {
@@ -33,6 +29,8 @@ export class EventPresentationComponent implements OnInit {
       .subscribe(
         eventsDto => {
           this.eventDto = eventsDto[0];
+          document.getElementById("text")
+            .innerHTML = this.eventDto.presentation;
         }
       );
   }
