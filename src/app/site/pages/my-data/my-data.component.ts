9a5ab12f4f70d39f3d836b718b3fae7279a21f08
rafaelspa <rsdepaula67@gmail.com>
2022-08-18 16:26:43 -0300

    refactor: my data component

--- src/app/site/pages/my-data/my-data.component.ts
+++ src/app/site/pages/my-data/my-data.component.ts
@@ -12,7 +12,6 @@ import { first } from "rxjs";
 })
 export class MyDataComponent implements OnInit {
   accountDto: AccountDto;
-  requestLoading: boolean = false;
 
   constructor(
     private router: Router,
