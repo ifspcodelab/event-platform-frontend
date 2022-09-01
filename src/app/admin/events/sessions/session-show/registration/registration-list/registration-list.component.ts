e3cf8a124fc049d6d106f8eb75046ff589ae74e7
ana004 <paulaana08002019@gmail.com>
2022-09-01 09:34:16 -0300

    feat: add list of subscribers in session show

--- src/app/admin/events/sessions/session-show/registration/registration-list/registration-list.component.ts
+++ src/app/admin/events/sessions/session-show/registration/registration-list/registration-list.component.ts
@@ -1,5 +1,5 @@
 import { SessionDto } from './../../../../../../core/models/activity.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { EnrolledStatusModel } from 'src/app/core/models/enrolled-status.model';
 import { SpaceType } from 'src/app/core/models/spaceType.model';
 
@@ -9,36 +9,38 @@ import { SpaceType } from 'src/app/core/models/spaceType.model';
   styleUrls: ['./registration-list.component.scss']
 })
 export class RegistrationListComponent implements OnInit {
+  @Input()
+  sessionId: string;
   displayedColumns: string[] = ['user', 'status'];
 
- enrolledsDto: any[] = [
-  {
-    id: "459736c1-159f-4de5-82dd-db66bd9a9079",
-    user: { id: "", name: "Carlos", email: "", cpf: "", agreed: "", role: "", verified: "true", registrationTimestamp: "" },
-    status: EnrolledStatusModel.CONFIRMED,
-  },
-  {
-    id: "459736c1-159f-4de5-82dd-db66bd9a9078",
-    user: { id: "", name: "Daniel", email: "", cpf: "", agreed: "", role: "", verified: "true", registrationTimestamp: "" },
-    status: EnrolledStatusModel.WAITING_LIST,
-  }
-]
-
-sessionDto: SessionDto = {
-  id: "",
-  title: "Sessão 1",
-  seats: 8,
-  schedulesSession: [
+  enrolledsDto: any[] = [
     {
-    id: "",
-    start: "21/09/2022 09:00",
-    end: "21/09/2022 11:00",
-    location: { id: "", name: "IFSP Campus São Paulo", address: "" },
-    area: { id: "", name: "Bloco C", reference: null },
-    space: { id: "", name: "Laboratório 2", capacity: 20, type: SpaceType.LABORATORY }
+      id: "459736c1-159f-4de5-82dd-db66bd9a9079",
+      user: { id: "", name: "Carlos", email: "", cpf: "", agreed: "", role: "", verified: "true", registrationTimestamp: "" },
+      status: EnrolledStatusModel.CONFIRMED,
+    },
+    {
+      id: "459736c1-159f-4de5-82dd-db66bd9a9078",
+      user: { id: "", name: "Daniel", email: "", cpf: "", agreed: "", role: "", verified: "true", registrationTimestamp: "" },
+      status: EnrolledStatusModel.WAITING_LIST,
     }
   ]
-}
+
+  sessionDto: SessionDto = {
+    id: "",
+    title: "Sessão 1",
+    seats: 8,
+    schedulesSession: [
+      {
+      id: "",
+      start: "21/09/2022 09:00",
+      end: "21/09/2022 11:00",
+      location: { id: "", name: "IFSP Campus São Paulo", address: "" },
+      area: { id: "", name: "Bloco C", reference: null },
+      space: { id: "", name: "Laboratório 2", capacity: 20, type: SpaceType.LABORATORY }
+      }
+    ]
+  }
 
   constructor() { }
 
