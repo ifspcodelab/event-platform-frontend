185bd126c33530d098234074d1065719cfca7d45
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-09-03 22:07:15 -0300

    feat: add session schedule id variable

--- src/app/admin/events/sessions/session-show/attendance-list/attendance-list.component.ts
+++ src/app/admin/events/sessions/session-show/attendance-list/attendance-list.component.ts
@@ -1,10 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {RegistrationStatus} from "../../../../../core/models/registration.status";
-import {SessionDto} from "../../../../../core/models/activity.model";
-import {SpaceType} from "../../../../../core/models/spaceType.model";
-import {RegistrationDto} from "../../../../../core/models/registration.model";
-import {RegistrationService} from "../../../../../core/services/registration.service";
-import {first} from "rxjs";
+import { Component, Input, OnInit } from '@angular/core';
+import { RegistrationStatus } from "../../../../../core/models/registration.status";
+import { SessionDto } from "../../../../../core/models/activity.model";
+import { SpaceType } from "../../../../../core/models/spaceType.model";
+import { RegistrationDto } from "../../../../../core/models/registration.model";
+import { RegistrationService } from "../../../../../core/services/registration.service";
+import { first } from "rxjs";
 
 @Component({
   selector: 'app-attendance-list',
@@ -18,26 +18,13 @@ export class AttendanceListComponent implements OnInit {
   checked = false;
   registrationsDto: RegistrationDto[] = [];
 
-  enrolledsDto: any[] = [
-    {
-      id: "459736c1-159f-4de5-82dd-db66bd9a9079",
-      user: { id: "", name: "Carlos", email: "", cpf: "", agreed: "", role: "", verified: "true", registrationTimestamp: "" },
-      status: RegistrationStatus.CONFIRMED,
-    },
-    {
-      id: "459736c1-159f-4de5-82dd-db66bd9a9078",
-      user: { id: "", name: "Daniel", email: "", cpf: "", agreed: "", role: "", verified: "true", registrationTimestamp: "" },
-      status: RegistrationStatus.WAITING_LIST,
-    }
-  ]
-
   sessionDto: SessionDto = {
     id: "",
     title: "Sessão 1",
     seats: 8,
     schedulesSession: [
       {
-        id: "",
+        id: "6bc14c1f-1677-4960-b88c-a2bd445b60d5",
         start: "21/09/2022 09:00",
         end: "21/09/2022 11:00",
         location: { id: "", name: "IFSP Campus São Paulo", address: "" },
@@ -46,6 +33,7 @@ export class AttendanceListComponent implements OnInit {
       }
     ]
   }
+
   schedulesSessionId = this.sessionDto.schedulesSession.map((obj) => obj.id);
   locationName = this.sessionDto.schedulesSession.map((obj) => obj.location.name);
   areaName = this.sessionDto.schedulesSession.map((obj) => obj.area.name);
@@ -64,6 +52,4 @@ export class AttendanceListComponent implements OnInit {
   ngOnInit(): void {
     this.fetchRegistrations();
   }
-
-
 }
