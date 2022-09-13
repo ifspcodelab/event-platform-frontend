34bc5fb419927f1e029e637ed9728b7616c4fc9a
rafaelspa <rsdepaula67@gmail.com>
2022-09-13 17:50:09 -0300

    feat: add event show component in organizer

---
+++ src/app/organizer/events/event-show/event-show.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { LoaderService } from "../../../admin/loader.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { EventService } from "../../../core/services/event.service";
+import { NotificationService } from "../../../core/services/notification.service";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatDialog } from "@angular/material/dialog";
+import { SubeventService } from "../../../core/services/subevent.service";
+import { SubeventDto } from "../../../core/models/subevent.model";
+import { HttpErrorResponse } from "@angular/common/http";
+import { AccountDto } from "../../../core/models/account.model";
+import { LiveAnnouncer } from "@angular/cdk/a11y";
+import { first } from "rxjs";
+import { EventDto } from "../../../core/models/event.model";
+
+@Component({
+  selector: 'app-event-show',
+  templateUrl: './event-show.component.html',
+  styleUrls: ['./event-show.component.scss']
+})
+export class EventShowComponent implements OnInit {
+  tabSelectedIndex: number = 0;
+  eventDto: EventDto;
+  eventId: string;
+  displayedColumns: string[] = ['title', 'status', 'startDate', 'endDate'];
+  subeventsDto: SubeventDto[] = [];
+  dataSource: MatTableDataSource<SubeventDto>;
+  accountDto: AccountDto;
+
+
+  constructor(
+    private eventService: EventService,
+    private subeventService: SubeventService,
+    private notificationService: NotificationService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private loaderService: LoaderService,
+    private _liveAnnouncer: LiveAnnouncer,
+  ) { }
+
+  ngOnInit(): void {
+    this.loaderService.show()
+    this.eventId = this.route.snapshot.paramMap.get('eventId');
+    this.fetchEvent(this.eventId);
+  }
+
+  fetchEvent(eventId: string) {
+    this.eventService.getEventById(eventId)
+      .pipe(first())
+      .subscribe(
+        eventDto => {
+          this.eventDto = eventDto;
+          this.fetchSubevents(this.eventId);
+          this.loaderService.hide();
+        }
+      );
+  }
+
+  fetchSubevents(eventId: string) {
+    this.subeventService.getSubevents(eventId)
+      .subscribe(subevents => {
+        this.subeventsDto = subevents;
+        this.dataSource = new MatTableDataSource<SubeventDto>(this.subeventsDto);
+        this.loaderService.hide();
+      });
+  }
+
+  openEventAndSubeventList() {
+    return this.router.navigate(['organizer']);
+  }
+
+  openSubeventShow(subeventDto: SubeventDto) {
+    return this.router.navigate(['organizer', 'sub-events', subeventDto.id]);
+  }
+
+  handleError(error: any) {
+    if(error instanceof HttpErrorResponse) {
+      if(error.status === 409) {
+        this.notificationService.error(error.error.violations[0].message);
+      }
+    }
+  }
+}
+
