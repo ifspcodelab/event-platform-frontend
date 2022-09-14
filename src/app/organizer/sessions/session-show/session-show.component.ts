3f3b3997ae3d68a7a53726bd101de8b06f32f609
rafaelspa <rsdepaula67@gmail.com>
2022-09-13 21:54:33 -0300

    feat: add session show component

--- src/app/organizer/sessions/session-show/session-show.component.ts
+++ src/app/organizer/sessions/session-show/session-show.component.ts
@@ -2,15 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { SessionDto } from "../../../core/models/activity.model";
 import { MatDialog } from "@angular/material/dialog";
 import { ActivatedRoute, Router } from "@angular/router";
-import { SessionService } from "../../../core/services/session.service";
-import {
-  ConfirmationDialogComponent
-} from "../../../core/components/confirmation-dialog/confirmation-dialog.component";
 import { NotificationService } from "../../../core/services/notification.service";
 import { HttpErrorResponse } from "@angular/common/http";
-import { CancelDialogComponent } from "../../../core/components/cancel-dialog/cancel-dialog.component";
 import { first } from "rxjs";
-import { CancellationMessageCreateDto } from "../../../core/models/event.model";
+import { OrganizerAreaService } from "../../../core/services/organizer-area.service";
 
 @Component({
   selector: 'app-session-show',
@@ -18,18 +13,13 @@ import { CancellationMessageCreateDto } from "../../../core/models/event.model";
   styleUrls: ['./session-show.component.scss']
 })
 export class SessionShowComponent implements OnInit {
-  eventMode: boolean = true;
   eventId: string = null;
   subeventId: string = null;
-  activityId: string;
   sessionId: string;
   sessionDto: SessionDto = null;
-  cancellationMessageCreateDto: CancellationMessageCreateDto;
-
-
 
   constructor(
-    private sessionService: SessionService,
+    private organizerAreaService: OrganizerAreaService,
     private router: Router,
     private route: ActivatedRoute,
     public dialog: MatDialog,
@@ -39,11 +29,9 @@ export class SessionShowComponent implements OnInit {
   ngOnInit(): void {
     this.eventId = this.route.snapshot.paramMap.get('eventId');
     this.subeventId = this.route.snapshot.paramMap.get('subeventId');
-    this.activityId = this.route.snapshot.paramMap.get('activityId');
     this.sessionId = this.route.snapshot.paramMap.get('sessionId');
 
     if(this.subeventId) {
-      this.eventMode = false;
       this.fetchSubEventSession();
     } else {
       this.fetchEventSession();
@@ -51,7 +39,7 @@ export class SessionShowComponent implements OnInit {
   }
 
   private fetchSubEventSession() {
-    this.sessionService.getSubEventSession(this.eventId, this.subeventId, this.activityId, this.sessionId)
+    this.organizerAreaService.getSubeventSession(this.subeventId, this.sessionId)
       .pipe(first())
       .subscribe(sessionDto => {
         this.sessionDto = sessionDto;
@@ -59,7 +47,7 @@ export class SessionShowComponent implements OnInit {
   }
 
   private fetchEventSession() {
-    this.sessionService.getEventSession(this.eventId, this.activityId, this.sessionId)
+    this.organizerAreaService.getEventSession(this.eventId, this.sessionId)
       .pipe(first())
       .subscribe(sessionDto => {
         this.sessionDto = sessionDto;
@@ -67,105 +55,16 @@ export class SessionShowComponent implements OnInit {
   }
 
   backLink() {
-    if(this.eventMode) {
+    if(this.subeventId) {
       return this.router.navigate(
-        ['admin', 'events', this.eventId, 'activities', this.activityId], { queryParams: { tab: 2 } }
+        ['organizer', 'sub-events', this.subeventId, 'sessions']
       );
     } else {
       return this.router.navigate(
-        ['admin', 'events', this.eventId, 'sub-events', this.subeventId, 'activities', this.activityId], { queryParams: { tab: 2 } }
+        ['organizer', 'events', this.eventId, 'sessions']
       );
     }
-
   }
-
-  openCancelDialog() {
-    const dialogRef = this.dialog.open(CancelDialogComponent, {
-      width: '400px',
-      data: { name: "Sessão", cancelMessage: this.cancellationMessageCreateDto, cancelText: "Fechar", okText: "Cancelar"},
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if(result) {
-        this.cancellationMessageCreateDto = result;
-        return this.eventMode ? this.cancelEventSession() : this.cancelSubEventSession();
-      }
-    });
-  }
-
-  cancelEventSession() {
-    this.sessionService.cancelEventSession(this.eventId, this.activityId, this.sessionId, this.cancellationMessageCreateDto)
-      .pipe(first())
-      .subscribe({
-        next: sessionDto => {
-          this.notificationService.success("Sessão cancelada com sucesso")
-          this.sessionDto = sessionDto;
-        },
-        error: error => this.handleError(error)
-      });
-  }
-
-  cancelSubEventSession() {
-    this.sessionService.cancelSubEventSession(this.eventId, this.subeventId, this.activityId,  this.sessionId, this.cancellationMessageCreateDto)
-      .pipe(first())
-      .subscribe({
-        next: sessionDto => {
-          this.notificationService.success("Sessão cancelada com sucesso")
-          this.sessionDto = sessionDto;
-        },
-        error: error => this.handleError(error)
-      });
-  }
-
-  private getConfirmationDialogConfig() {
-    return {
-      autoFocus: true,
-      data: {
-        name: "Excluir sessão",
-        text: `A sessão ${this.sessionDto.title} será excluída de forma definitiva.`,
-        cancelText: "Cancelar",
-        okText: "Excluir"
-      }
-    }
-  }
-
-  openDeleteConfirmationDialog() {
-    this.dialog.open(ConfirmationDialogComponent, this.getConfirmationDialogConfig()).afterClosed()
-      .subscribe( result => {
-        if (result) {
-          if(this.eventMode) {
-            this.deleteEventSession();
-          } else {
-            this.deleteSubEventSession();
-          }
-        }
-      });
-  }
-
-  deleteEventSession() {
-    this.sessionService.deleteEventSession(this.eventId, this.activityId, this.sessionId)
-      .pipe(first())
-      .subscribe({
-        next: () => {
-          this.notificationService.success("Atividade excluida com sucesso");
-          this.backLink();
-        },
-        error: error => this.handleError(error)
-      });
-  }
-
-  deleteSubEventSession() {
-    this.sessionService.deleteSubEventSession(this.eventId, this.subeventId, this.activityId, this.sessionId)
-      .pipe(first())
-      .subscribe({
-        next: () => {
-          this.notificationService.success("Atividade excluida com sucesso");
-          this.backLink();
-        },
-        error: error => this.handleError(error)
-      });
-  }
-
 
   handleError(error: any) {
     if(error instanceof HttpErrorResponse) {
