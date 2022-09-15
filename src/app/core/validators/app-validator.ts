9205ff40c376f49e44cbdb311c2d2758483c2b57
Diego <diegocdsantos@gmail.com>
2022-09-15 17:40:35 -0300

    refactor: move invalidDomains array out of the function

--- src/app/core/validators/app-validator.ts
+++ src/app/core/validators/app-validator.ts
@@ -92,17 +92,18 @@ export class AppValidators {
 
   static isEmailDomainValid(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
-      const invalidDomains = ['@gnail.com', '@alino.ifsp.edu.br', '@aluno.ifsp.com.br'];
       const domainIndex = control.value.indexOf('@');
       if (domainIndex == -1) {
         return null;
       }
-      const domain = control.value.slice(domainIndex).trim();
-      const includes = invalidDomains.includes(domain);
+      const domain = control.value.slice(domainIndex);
+      const includes = this.invalidDomains.includes(domain);
       return includes
         ? { isemaildomainvalid: true }
         : null;
     };
   }
+
+  static invalidDomains = ['@gnail.com', '@alino.ifsp.edu.br', '@aluno.ifsp.com.br'];
 
 }
