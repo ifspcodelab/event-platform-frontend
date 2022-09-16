b327c714a2799b05b177ea6ed1a5fe4a9c452045
Domingos Latorre <domingos.latorre@gmail.com>
2022-09-16 13:32:45 -0300

    feat: cancel registration

--- src/app/admin/events/registrations/registration-list/registration-list.component.ts
+++ src/app/admin/events/registrations/registration-list/registration-list.component.ts
@@ -5,7 +5,9 @@ import { RegistrationDto, RegistrationStatus } from "../../../../core/models/reg
 import { Router } from "@angular/router";
 import { LiveAnnouncer } from "@angular/cdk/a11y";
 import { RegistrationService } from "../../../../core/services/registration.service";
-import { SessionScheduleDto } from "../../../../core/models/activity.model";
+import { ConfirmationDialogComponent } from "../../../../core/components/confirmation-dialog/confirmation-dialog.component";
+import { MatDialog } from "@angular/material/dialog";
+import { SessionDto } from "../../../../core/models/activity.model";
 
 @Component({
   selector: 'app-registration-list',
@@ -22,20 +24,50 @@ export class RegistrationListComponent implements OnInit {
   @Input()
   sessionId: string;
   @Input()
+  session: SessionDto;
+  @Input()
   registrationsDto: RegistrationDto[] = [];
-  displayedColumns: string[] = ['date', 'account.name', 'account.email', 'status', 'timeEmailWasSent'];
   dataSource: MatTableDataSource<RegistrationDto>;
   @ViewChild(MatSort)
   sort: MatSort;
+
+  confirmedListDto: RegistrationDto[] = [];
+  confirmedColumns: string[] = ['date', 'account.name', 'account.cpf', 'action'];
+
+  waitingListDisplayedColumns: string[] = ['date', 'account.name', 'account.cpf', 'action'];
+  waitingListDto: RegistrationDto[] = [];
+
+  canceledListDisplayedColumns: string[] = ['date', 'account.name', 'account.cpf', 'status'];
+  canceledListDto: RegistrationDto[] = [];
 
   constructor(
     private registrationService: RegistrationService,
     private router: Router,
     private _liveAnnouncer: LiveAnnouncer,
+    public dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
-    this.dataSource = new MatTableDataSource<RegistrationDto>(this.registrationsDto.sort((a,b) => this.sortByDate(a,b)));
+    this.confirmedListDto = this.registrationsDto
+      .filter(r => RegistrationStatus[r.registrationStatus] == RegistrationStatus.CONFIRMED.toString())
+      .sort((a,b) => this.sortByDate(a,b))
+    this.dataSource = new MatTableDataSource<RegistrationDto>(this.confirmedListDto);
+
+    this.waitingListDto = this.registrationsDto
+      .filter(r =>
+        RegistrationStatus[r.registrationStatus] == RegistrationStatus.WAITING_LIST.toString() ||
+        RegistrationStatus[r.registrationStatus] == RegistrationStatus.WAITING_LIST.toString()
+      )
+      .sort((a,b) => this.sortByDate(a,b));
+
+    this.canceledListDto = this.registrationsDto
+      .filter(r =>
+        RegistrationStatus[r.registrationStatus] == RegistrationStatus.CANCELED_BY_USER.toString() ||
+        RegistrationStatus[r.registrationStatus] == RegistrationStatus.CANCELED_BY_ADMIN.toString() ||
+        RegistrationStatus[r.registrationStatus] == RegistrationStatus.CANCELED_BY_SYSTEM.toString()
+      )
+      .sort((a,b) => this.sortByDate(a,b));
+
     this.setSortingDataAccessor();
   }
 
@@ -48,7 +80,7 @@ export class RegistrationListComponent implements OnInit {
       switch(property) {
         case 'date': return item.date;
         case 'account.name': return item.account.name;
-        case 'account.email': return item.account.email;
+        case 'account.cpf': return item.account.cpf;
         case 'status': return item.registrationStatus;
         case 'timeEmailWasSent': return item.timeEmailWasSent;
         default: return item.id;
@@ -84,5 +116,42 @@ export class RegistrationListComponent implements OnInit {
     } else {
       this._liveAnnouncer.announce('Ordenação removida');
     }
+  }
+
+  private getConfirmationDialogConfig(registration: RegistrationDto) {
+    return {
+      autoFocus: true,
+      data: {
+        name: "Cancelar Inscrição",
+        text: `A inscrição do participante  ${registration.account.name} será cancelada de forma definitiva.`,
+        cancelText: "Sair",
+        okText: "Cancelar Inscrição"
+      }
+    }
+  }
+
+  openCancelConfirmationDialog(registration: RegistrationDto) {
+    this.dialog.open(ConfirmationDialogComponent, this.getConfirmationDialogConfig(registration))
+      .afterClosed()
+      .subscribe(result => {
+        if(result) {
+          this.cancelRegistration(registration)
+        }
+      });
+  }
+
+  cancelRegistration(registration: RegistrationDto) {
+    console.log(registration)
+    const registrationDto = this.confirmedListDto.find(r => r.id != registration.id);
+    registrationDto.registrationStatus = RegistrationStatus.CANCELED_BY_ADMIN;
+
+    // this.dataSource = new MatTableDataSource<RegistrationDto>(this.confirmedListDto);
+  }
+
+
+  cancelDisabled(): boolean {
+    const sessionDate = new Date(this.session.sessionSchedules[0].executionStart.substr(0, 10).replace(/-/g, '/'));
+    const today = new Date();
+    return today < sessionDate
   }
 }
